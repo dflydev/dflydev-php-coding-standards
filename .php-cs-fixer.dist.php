025bd60a9f9f88661f3cf92f51c3ee57b45a216e
Zaher Ghaibeh <27624+zaherg@users.noreply.github.com>
2023-05-03 18:36:03 +0300

    Research best way to package and ship php cs fixer rules (#5)
    * Adding an initial composer.json file
    * using phpcs-fixer-config library from ergebnis
    * Adding a license generator

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,23 @@
+<?php 
+
+use Ergebnis\License;
+
+$license = License\Type\MIT::text(__DIR__ . '/LICENSE',
+    License\Range::since(
+        License\Year::fromString(date('Y')),
+        new \DateTimeZone('UTC')
+    ),
+    License\Holder::fromString('Dragonfly Development Inc'),
+    License\Url::fromString('https://github.com/dflydev/dflydev-php-coding-standards')
+);
+
+$license->save();
+
+use Ergebnis\PhpCsFixer\Config;
+
+$config = Config\Factory::fromRuleSet(new Dflydev\PhpCsFixer\Config\RuleSet\Dflydev($license->header()));
+
+$config->getFinder()->in(__DIR__);
+$config->setCacheFile(__DIR__ . '/.php-cs-fixer.cache');
+
+return $config;    
